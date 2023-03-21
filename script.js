2f42ad98328f365c4f0b9c19e2c724b50146177b
Michal Gawlas <gawlas05@gmail.com>
2023-03-21 18:42:25 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/tasks';
+  var apiRoot = 'https://michalgawlaskodillaapp.michal2220.repl.co/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
