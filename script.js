9c6dc24f10190dcdbbe6bb172667d92cc8d80cf5
Michał Gawlas <117166081+michal2220@users.noreply.github.com>
2023-06-27 20:45:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://michalgawlaskodillaapp.michal2220.repl.co/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const trelloApiRoot = 'https://michalgawlaskodillaapp.michal2220.repl.co/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
